03cde1b27b07c6fb8cced3b2058785cff19630bf
vwhwang <vwhwang38@gmail.com>
2024-05-26 13:28:51 -0700

    chapter2Challenge

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,27 +1,65 @@
 package com.example.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
 
+    private val questionBank = listOf(
+        Questions(R.string.questionOne, true),
+        Questions(R.string.questionTwo,false),
+        Questions(R.string.questionThree, true)
+    )
+    private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.questionText.setText(questionBank[currentIndex].question)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        binding.questionText.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            showQuestion()
+        }
 
-        trueButton.setOnClickListener {
-            Snackbar.make(it, R.string.correct_toast, Snackbar.LENGTH_SHORT).show()
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            showQuestion()
         }
-        falseButton.setOnClickListener {
-            Snackbar.make(it, R.string.correct_toast, Snackbar.LENGTH_SHORT).show()
+        binding.previousButton.setOnClickListener {
+            if (currentIndex == 0) {
+                Toast.makeText(this, "no previous question", Toast.LENGTH_SHORT).show()
+            } else {
+                currentIndex = (currentIndex -1) % questionBank.size
+                showQuestion()
+            }
         }
+
+        binding.trueButton.setOnClickListener { view ->
+            checkAnswer(view, true)
+        }
+        binding.falseButton.setOnClickListener { view ->
+            checkAnswer(view, false)
+        }
+    }
+
+    private fun showQuestion() {
+        val nextQuestion = questionBank[currentIndex].question
+        binding.questionText.setText(nextQuestion)
+    }
+
+    private fun checkAnswer(view: View, userInput:Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userInput == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Snackbar.make(view, messageResId, Snackbar.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/example/geoquiz/Questions.kt
@@ -0,0 +1,5 @@
+package com.example.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Questions(@StringRes val question: Int, val answer: Boolean)
